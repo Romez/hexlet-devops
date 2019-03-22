4e96bb339975f81f2eab3d81b6b22e06cc317af4
roman_ushakov <roman.ushakov.89@inbox.ru>
2019-03-22 22:23:19 +0300

    add app service

---
+++ make-app.mk
@@ -0,0 +1,13 @@
+USER = "$(shell id -u):$(shell id -g)"
+
+app:
+	docker-compose up
+
+app-build:
+	docker-compose build
+
+app-bash:
+	docker-compose run --rm --user=$(USER) app bash
+
+app-setup: app-build
+	docker-compose run --user=$(USER) app composer install
