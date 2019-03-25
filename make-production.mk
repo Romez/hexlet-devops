4388038be13acd79b45076096221b12d9363e59c
roman_ushakov <roman.ushakov.89@inbox.ru>
2019-03-25 08:54:55 +0300

    add terraform docker machine

--- make-production.mk
+++ make-production.mk
@@ -1,2 +1,10 @@
+U := root
+
+production-setup:
+	ansible-playbook ansible/site.yml -i ansible/production -u $U --ask-vault-pass
+
 production-setup-env:
 	ansible-playbook ansible/production.yml -i ansible/production -vv --ask-vault-pass
+
+production-deploy:
+	ansible-playbook ansible/deploy.yml -i ansible/production -u $U --ask-vault-pass
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,15 @@ provider "digitalocean" {
   token = "${var.do_token}"
 }
 
+resource "digitalocean_ssh_key" "roman" {
+  name       = "roman"
+  public_key = "${file("./files/roman.pub")}"
+}
+
 resource "digitalocean_droplet" "web" {
-  image  = "ubuntu-18-04-x64"
+  image  = "docker-18-04"
   name   = "web-1"
   region = "sgp1"
   size   = "s-1vcpu-1gb"
+  ssh_keys = ["${digitalocean_ssh_key.roman.fingerprint}"]
 }
