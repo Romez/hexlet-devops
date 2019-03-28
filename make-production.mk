dd74c030d7e260343b8908f8c9ff7cf8f5939679
roman <roman.ushakov.89@inbox.ru>
2019-03-28 16:07:31 +0300

    test db

--- make-production.mk
+++ make-production.mk
@@ -5,7 +5,7 @@ production-setup:
 	docker-compose run --user=$(USER) app php artisan key:generate
 
 production-setup-env:
-	ansible-playbook ansible/production.yml -i ansible/production -vv --ask-vault-pass
+	ansible-playbook ansible/production.yml -i ansible/production -u $U --ask-vault-pass
 
 production-deploy:
 	ansible-playbook ansible/deploy.yml -i ansible/production -u $U --ask-vault-pass
