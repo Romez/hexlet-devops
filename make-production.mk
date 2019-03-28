bdbf968e187fe48f4cca2cbb0fe8499ffa47f9c3
roman_ushakov <roman.ushakov.89@inbox.ru>
2019-03-28 09:45:49 +0300

    fix test

--- make-production.mk
+++ make-production.mk
@@ -2,6 +2,7 @@ U := root
 
 production-setup:
 	ansible-playbook ansible/site.yml -i ansible/production -u $U --ask-vault-pass
+	docker-compose run --user=$(USER) app php artisan key:generate
 
 production-setup-env:
 	ansible-playbook ansible/production.yml -i ansible/production -vv --ask-vault-pass
